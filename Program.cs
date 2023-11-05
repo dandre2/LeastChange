e9449d091951a262e9b0a6deb9ffc84db7cadd5b
dandre2 <roman.adamsv2@gmail.com>
2023-11-05 10:23:09 -0600

    made the list a dictionary

--- Program.cs
+++ Program.cs
@@ -1,30 +1,34 @@
-﻿//create a list of coins and bills of type double
-//set value remaining as user input
-//check for the greatest value that is less than or equal to value remaining
-//subtract that value from value remaing and add that value as many times as needed to the right side of the list
-// keep checking until value remaining equals zero
-//print list
-
+﻿//get user input
 Console.WriteLine("how much money would you like");
 double userInput = double.Parse(Console.ReadLine());
-List<double> BillsAndCoins = new List<double>()
+//create a dictionary of coins and bills of type double, int
+
+Dictionary<double, int> BillsAndCoins = new Dictionary<double, int>()
 {
-    100,
-    50,
-    20,
-    10,
-    5,
-    1,
-    0.25,
-    0.1,
-    0.05,
-    0.01
+    { 100, 0},
+    { 50,0 },
+    { 20,0 },
+    { 10,0 },
+    { 5,0 },
+    { 1,0 },
+    { 0.25,0 },
+    { 0.1,0 },
+    { 0.05,0 },
+    { 0.01,0 },
 };
+
+//set value remaining as user input
 double valueRemaining = userInput;
+
+// check for the greatest value that is less than or equal to value remaining
+// subtract that value from value remaing and add that value as many times as needed to the right side of the list
+// keep checking until value remaining equals zero
+
 
 for (int i = 0; i <= BillsAndCoins.Count; i++)
 {
-    Console.WriteLine(BillsAndCoins[i]);
-    Console.WriteLine(valueRemaining == BillsAndCoins[i]);
-
+    //print list
+   Console.WriteLine(BillsAndCoins[i]);
+   Console.WriteLine(valueRemaining == BillsAndCoins[i]);
+    
 }
