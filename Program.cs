2877114c6d4ec903724b08207ec7291323b74e31
dandre2 <roman.adamsv2@gmail.com>
2023-11-05 10:48:34 -0600

    added decimal to the code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿//get user input
 Console.WriteLine("how much money would you like");
-double userInput = double.Parse(Console.ReadLine());
+decimal userInput = decimal.Parse(Console.ReadLine());
 //create a dictionary of coins and bills of type double, int
 
-Dictionary<double, int> BillsAndCoins = new Dictionary<double, int>()
+Dictionary<decimal, int> BillsAndCoins = new Dictionary<decimal, int>()
 {
     { 100, 0},
     { 50,0 },
@@ -11,20 +11,27 @@ Dictionary<double, int> BillsAndCoins = new Dictionary<double, int>()
     { 10,0 },
     { 5,0 },
     { 1,0 },
-    { 0.25,0 },
-    { 0.1,0 },
-    { 0.05,0 },
-    { 0.01,0 },
+    { 0.25m,0 },
+    { 0.1m,0 },
+    { 0.05m,0 },
+    { 0.01m,0 },
 };
 
 //set value remaining as user input
-double valueRemaining = userInput;
+decimal valueRemaining = userInput;
 
 // check for the greatest value that is less than or equal to value remaining
 // subtract that value from value remaing and add that value as many times as needed to the right side of the list
 // keep checking until value remaining equals zero
-
-
+//while (valueRemaining > 0) {
+foreach (var keyValue in BillsAndCoins)
+{
+    while (valueRemaining>=keyValue.Key) 
+    {
+        valueRemaining -= keyValue.Key;
+        BillsAndCoins[keyValue.Key] += 1;
+    }
+}
 
 foreach (var keyValue in BillsAndCoins)
 {
